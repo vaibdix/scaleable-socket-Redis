16ff63afb0d95489aab93576c6606b59efc13133
vaibdix <dixitvaib@gmail.com>
2023-12-26 21:55:00 +0530

    kafka to prisma data send success

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,60 +1,62 @@
 import { Server } from "socket.io";
 import Redis from "ioredis";
-
+import prismaClient from "./prisma";
+import { produceMessage } from "./kafka";
+ 
 const pub = new Redis({
-    host: "redis-1d685ee7-vaibdix1-a29f.a.aivencloud.com",
-    port: 26905,
-    username: "default",
-    password: "AVNS_7i3R-2JvT8W_ftxUAG-",
+    host: "",
+    port: ,
+    username: "",
+    password: "",
 });
 
 const sub = new Redis({
-    host: "redis-1d685ee7-vaibdix1-a29f.a.aivencloud.com",
-    port: 26905,
-    username: "default",
-    password: "AVNS_7i3R-2JvT8W_ftxUAG-",
+    host: "",
+    port: ,
+    username: "",
+    password: "",
 });
 
 class SocketService {
-  private _io: Server;
-
+    private _io: Server;
+  
     constructor() {
-        console.log("Init Socket Service...");
-        this._io = new Server({
-            cors: {
-                allowedHeaders: ["*"],
-                origin: "*",
-            },
-        });
-        sub.subscribe("MESSAGES");
+      console.log("Init Socket Service...");
+      this._io = new Server({
+        cors: {
+          allowedHeaders: ["*"],
+          origin: "*",
+        },
+      });
+      sub.subscribe("MESSAGES");
     }
-
+  
     public initListeners() {
-            const io = this.io;
-            console.log("Init Socket Listeners...");
-
-        io.on("connect", (socket) => {
-            console.log(`New Socket Connected`, socket.id);
-
-            socket.on("event:message", async ({ message }: { message: string }) => {
-                console.log("New Message Rec.", message);
-                // publish this message to redis
-                await pub.publish("MESSAGES", JSON.stringify({ message }));
-            });
+      const io = this.io;
+      console.log("Init Socket Listeners...");
+  
+      io.on("connect", (socket) => {
+        console.log(`New Socket Connected`, socket.id);
+        socket.on("event:message", async ({ message }: { message: string }) => {
+          console.log("New Message Rec.", message);
+          // publish this message to redis
+          await pub.publish("MESSAGES", JSON.stringify({ message }));
         });
-
-        sub.on("message", async (channel, message) => {
-            if (channel === "MESSAGES") {
-                console.log("new message from redis", message);
-                io.emit("message", message);
-            }
-          });
-
+      });
+  
+      sub.on("message", async (channel, message) => {
+        if (channel === "MESSAGES") {
+          console.log("new message from redis", message);
+          io.emit("message", message);
+          await produceMessage(message);
+          console.log("Message Produced to Kafka Broker");
+        }
+      });
+    }
+  
+    get io() {
+      return this._io;
+    }
   }
-
-  get io() {
-    return this._io;
-  }
-}
-
-export default SocketService;
+  
+  export default SocketService;
