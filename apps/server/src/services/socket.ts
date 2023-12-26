a8689720e76e4acb7cfebe771694bfa3e94e771e
vaibdix <dixitvaib@gmail.com>
2023-12-26 11:41:32 +0530

    pub/sub added

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,38 +1,60 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const pub = new Redis({
+    host: "redis-1d685ee7-vaibdix1-a29f.a.aivencloud.com",
+    port: 26905,
+    username: "default",
+    password: "AVNS_7i3R-2JvT8W_ftxUAG-",
+});
+
+const sub = new Redis({
+    host: "redis-1d685ee7-vaibdix1-a29f.a.aivencloud.com",
+    port: 26905,
+    username: "default",
+    password: "AVNS_7i3R-2JvT8W_ftxUAG-",
+});
 
 class SocketService {
-    private _io: Server;
+  private _io: Server;
+
     constructor() {
-        console.log("Init Socket Service...")
+        console.log("Init Socket Service...");
         this._io = new Server({
             cors: {
-                allowedHeaders: ['*'],
-                origin: '*'
-            }
+                allowedHeaders: ["*"],
+                origin: "*",
+            },
         });
-        
-    }
-
-    get io() {
-        return this._io;
+        sub.subscribe("MESSAGES");
     }
 
     public initListeners() {
-        const io = this.io;
-        console.log('Init Socket Listeners')
+            const io = this.io;
+            console.log("Init Socket Listeners...");
 
-        io.on('connect', (socket) => {
+        io.on("connect", (socket) => {
             console.log(`New Socket Connected`, socket.id);
 
-            socket.on('event:message', async ({message}: {message: string}) => {
-                console.log('New Message Rec', message)
-            })
+            socket.on("event:message", async ({ message }: { message: string }) => {
+                console.log("New Message Rec.", message);
+                // publish this message to redis
+                await pub.publish("MESSAGES", JSON.stringify({ message }));
+            });
         });
-    }
+
+        sub.on("message", async (channel, message) => {
+            if (channel === "MESSAGES") {
+                console.log("new message from redis", message);
+                io.emit("message", message);
+            }
+          });
+
+  }
+
+  get io() {
+    return this._io;
+  }
 }
 
 export default SocketService;
-
-function async(arg0: { message: any; }, arg1: { message: any; }): (...args: any[]) => void {
-    throw new Error("Function not implemented.");
-}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,20 +4,16 @@ import { useSocket } from "../context/SocketProvider";
 import classes from "./page.module.css";
 
 export default function Page() {
-  const { sendMessage } = useSocket();
+  const { sendMessage, messages } = useSocket();
   const [message, setMessage] = useState("");
 
   return (
     <div>
       <div>
-        <h1>All msg will appear here</h1>
-      </div>
-      <div>
         <input
           onChange={(e) => setMessage(e.target.value)}
           className={classes["chat-input"]}
-          type="text"
-          placeholder="Message"
+          placeholder="Message..."
         />
         <button
           onClick={(e) => sendMessage(message)}
@@ -25,6 +21,11 @@ export default function Page() {
         >
           Send
         </button>
+      </div>
+      <div>
+        {messages.map((e) => (
+          <li>{e}</li>
+        ))}
       </div>
     </div>
   );
